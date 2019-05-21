54940216ec7468d75643321cfcb4e6ace090da8f
Daniel Thompson <DanThomp507@gmail.com>
2019-05-21 17:20:29 -0400

    wrote random time function

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const holes = document.querySelectorAll('.hole');
 const score = document.querySelectorAll('.score');
 const moles = document.querySelectorAll('.mole');
+
+let score = 0;
+let lastHole;
+let timeUp = false;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
